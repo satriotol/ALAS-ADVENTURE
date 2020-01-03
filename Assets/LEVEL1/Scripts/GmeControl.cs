a111b7f87a68863d7f037aee5addd6c532d68c9a
Satrio <satriotol69@gmail.com>
2020-01-04 02:22:30 +0700

    traffic light

---
+++ Assets/LEVEL1/Scripts/GmeControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GmeControl : MonoBehaviour
+{
+    public GameObject green;
+    public GameObject red;
+
+    /*IEnumerator Start()
+    {
+        yield return StartCoroutine(WaitAndPrint(10.0f));
+    }
+    */
+    void Start()
+    {
+        StartCoroutine(WaitAndPrint());
+    }
+    IEnumerator WaitAndPrint()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(5f);
+            red.SetActive(false);
+            green.SetActive(true);
+            yield return new WaitForSeconds(5f);
+            red.SetActive(true);
+            green.SetActive(false);
+
+        }
+
+    }
+    /*public void Enable()
+    {
+        green.SetActive(true);
+    }
+    public void Disable()
+    {
+        green.SetActive(false);
+    }
+    */
+}
