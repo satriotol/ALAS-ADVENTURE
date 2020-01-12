49c59e410b560acee19d84f85db088f14717518b
aziz <alazizyiiha@gmail.com>
2020-01-12 16:38:04 +0700

    1

--- Assets/111.cs
+++ Assets/111.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class jjhujiji : MonoBehaviour
+public class 111 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
