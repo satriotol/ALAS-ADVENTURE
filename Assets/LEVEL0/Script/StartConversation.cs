2035730a7e0f4fae313563764185cf5b561a53b0
Satrio <satriotol69@gmail.com>
2020-01-17 11:19:38 +0700

    dialogue start

---
+++ Assets/LEVEL0/Script/StartConversation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartConversation : MonoBehaviour
+{
+    public bool Conversation;
+    public LayerMask targetlayer;
+    public Transform deteksilampu;
+    public float jangkauan;
+    public GameObject DialogueCanvas;
+
+    void Update()
+    {
+        Conversation = Physics2D.OverlapCircle(deteksilampu.position, jangkauan, targetlayer);
+
+
+        if (Conversation == true)
+        {
+            Debug.Log("Kena Gan");
+            //SceneManager.LoadScene(0);
+            DialogueCanvas.SetActive(true);
+        }
+        else
+        {
+            DialogueCanvas.SetActive(false);
+        }
+    }
+}
