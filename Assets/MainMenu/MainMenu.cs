9f5463d720e8017d229917db5769cc2e0c130643
Satrio <satriotol69@gmail.com>
2020-01-22 03:30:44 +0700

    lose kondisi

--- Assets/MainMenu/MainMenu.cs
+++ Assets/MainMenu/MainMenu.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        Application.LoadLevel(0);
+    }
     public void PlayGame ()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
