fef5ebee54d9c54b2036804e014d024792a0d0b0
Satrio <satriotol69@gmail.com>
2020-01-22 01:10:09 +0700

    level 1 kondisi menang

---
+++ Assets/gameover/Openit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Openit : MonoBehaviour
+
+{
+    public bool Win;
+    public LayerMask targetlayer;
+    public Transform deteksiwin;
+    public float jangkauan;
+    public GameObject DialogueCanvas;
+
+    private void Update()
+    {
+        Win = Physics2D.OverlapCircle(deteksiwin.position, jangkauan, targetlayer);
+
+
+        if (Win == true)
+        {
+            DialogueCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            DialogueCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+}
