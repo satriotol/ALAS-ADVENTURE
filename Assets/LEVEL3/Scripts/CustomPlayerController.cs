79a05fe4a95868cbc9c31354faa05a47e102b5be
Satrio <satriotol69@gmail.com>
2020-01-25 20:00:23 +0700

    android level 0

--- Assets/LEVEL3/Scripts/CustomPlayerController.cs
+++ Assets/LEVEL3/Scripts/CustomPlayerController.cs
@@ -26,9 +26,15 @@ public class CustomPlayerController : MonoBehaviour
 
 	//Start point of the player
 	public string startPoint;
+    public bool tombolkiri, tombolkanan, tombolatas, tombolbawah;
 
-	// Use this for initialization
-	void Start()
+
+    public bool balik;
+    public int pindah;
+
+
+    // Use this for initialization
+    void Start()
 	{
 
 		//I get the animator in the player
@@ -47,74 +53,114 @@ public class CustomPlayerController : MonoBehaviour
 		//If the player it's not attacking he does not move
 		if (!playerAttack)
 		{
-			//Here i make an tranlate to move the player getting the axis
+            //Here i make an tranlate to move the player getting the axis
 
-			//Moving to the right and the left
-			if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
-			{
-				//transform.Translate (new Vector3 (Input.GetAxisRaw ("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+            //Moving to the right and the left
+            if (Input.GetKey(KeyCode.A)| (tombolkiri==true))
+            {
+                transform.Translate(Vector2.right * -moveSpeed * Time.deltaTime);
+               playerMoving = true;
+            }
+            if (Input.GetKey(KeyCode.D) || (tombolkanan == true))
+            {
+                transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+                playerMoving = true;
+            }
+            if (Input.GetKey(KeyCode.W) || (tombolatas == true))
+            {
+                transform.Translate(Vector2.up * moveSpeed * Time.deltaTime,0f);
+                playerMoving = true;
+            }
+            if (Input.GetKey(KeyCode.S) || (tombolbawah == true))
+            {
+                transform.Translate(Vector2.up * -moveSpeed * Time.deltaTime,0f);
+                playerMoving = true;
+            }
 
-				//With the rigid body i don't need the tranlate and this removes the bounce with the collider
-				playerRigidBody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * currentMoveSpeed, playerRigidBody.velocity.y);
-				playerMoving = true;
-				lastMovement = new Vector2(Input.GetAxisRaw("Horizontal"), 0f);
-			}
+   //         if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+			//{
+			//	//transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
 
-			//Moving up and Down
-			if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
-			{
-				//transform.Translate (new Vector3 (0f, Input.GetAxisRaw ("Vertical") * moveSpeed * Time.deltaTime, 0f));
+			//	playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
+			//	playerMoving = true;
+			//	lastMovement = new Vector2(0f, Input.GetAxisRaw("Vertical"));
+			//}
 
-				playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, Input.GetAxisRaw("Vertical") * currentMoveSpeed);
-				playerMoving = true;
-				lastMovement = new Vector2(0f, Input.GetAxisRaw("Vertical"));
-			}
+            //If the player it's not moving it stops
+            if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+                playerRigidBody.velocity = new Vector2(0f, playerRigidBody.velocity.y);
 
-			//If the player it's not moving it stops
-			if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
-				playerRigidBody.velocity = new Vector2(0f, playerRigidBody.velocity.y);
-
-			if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+            if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
 				playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, 0f);
 
-			//If i press Z he attacks
-			if (Input.GetKeyDown(KeyCode.Z))
-			{
-				playerAttackTimeCounter = playerAttackTime;
-				playerAttack = true;
-				playerRigidBody.velocity = Vector2.zero;
-				playerAnim.SetBool("PlayerAttacking", true);
-			}
+            //Takes the valor of the axis and return in absolute, so i know if it's 1 or 0
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
+            {
+                //If there's any movement will slow 'cause we're moving diagonal
+                currentMoveSpeed = moveSpeed * diagonalMoveModifier;
+            }
+            else
+            {
+                currentMoveSpeed = moveSpeed;
+            }
+            if (pindah > 0 && !balik)
+            {
+                balikbadan();
+            }
+            else if (pindah < 0 && balik)
+            {
+                balikbadan();
+            }
 
-			//Takes the valor of the axis and return in absolute, so i know if it's 1 or 0
-			if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.5f && Mathf.Abs(Input.GetAxisRaw("Vertical")) > 0.5f)
-			{
-				//If there's any movement will slow 'cause we're moving diagonal
-				currentMoveSpeed = moveSpeed * diagonalMoveModifier;
-			}
-			else
-			{
-				currentMoveSpeed = moveSpeed;
-			}
-		}
-
-		//When the counter it's greater he will go back to 0
-		if (playerAttackTimeCounter > 0)
-		{
-			playerAttackTimeCounter -= Time.deltaTime;
-		}
-		//Resets the player Animation
-		if (playerAttackTimeCounter <= 0)
-		{
-			playerAttack = false;
-			playerAnim.SetBool("PlayerAttacking", false);
-		}
+        }
 
 		//Here it's where i ser the animator
-		playerAnim.SetFloat("MoveX", Input.GetAxisRaw("Horizontal"));
-		playerAnim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
+        playerAnim.SetFloat("MoveX",Input.GetAxisRaw("Horizontal"));
+        playerAnim.SetFloat("MoveY", Input.GetAxisRaw("Vertical"));
 		playerAnim.SetBool("PlayerMoving", playerMoving);
 		playerAnim.SetFloat("LastMoveX", lastMovement.x);
 		playerAnim.SetFloat("LastMoveY", lastMovement.y);
+
+        
 	}
+    void balikbadan()
+    {
+        balik = !balik;
+        Vector3 karakter = transform.localScale;
+        karakter.x *= -1;
+        transform.localScale = karakter;
+    }
+
+    public void tekankiri()
+    {
+        tombolkiri = true;
+    }
+    public void lepaskiri()
+    {
+        tombolkiri = false;
+    }
+    public void tekankanan()
+    {
+        tombolkanan = true;
+    }
+    public void lepaskanan()
+    {
+        tombolkanan = false;
+    }
+    public void tekanatas()
+    {
+        tombolatas = true;
+    }
+    public void lepasatas()
+    {
+        tombolatas = false;
+    }
+    public void tekanbawah()
+    {
+        tombolbawah = true;
+    }
+    public void lepasbawah()
+    {
+        tombolbawah = false;
+    }
 }
